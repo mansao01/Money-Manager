a890eedbfb2287f8c6fd98b6aa64c9fbc2380a4a
Novri Lukman Zyarif <novrilukman15@gmail.com>
2021-11-27 21:07:00 +0700

    Add Splash Screen

--- app/src/main/java/com/mansao/moneymanager/MainActivity.kt
+++ app/src/main/java/com/mansao/moneymanager/MainActivity.kt
@@ -1,11 +1,23 @@
  package com.mansao.moneymanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val delayMilliSecond : Long= 3000
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, delayMilliSecond)
     }
 }
