9bd953e1f1ae7f7c99ee383085453b068dc43345
Novri Lukman Zyarif <novrilukman15@gmail.com>
2021-11-29 15:00:09 +0700

    add model

---
+++ app/src/main/java/com/mansao/moneymanager/database/Money.kt
@@ -0,0 +1,12 @@
+package com.mansao.moneymanager.database
+
+
+data class Money(
+    var id: Int = 0,
+
+    var title: String? = null,
+
+    var description: String? = null,
+
+    var date : String? = null
+)
